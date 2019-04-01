dd618a08d01997401b26ac3d728218f24d4d796a
Tom Quinn <tomquinn@hotmail.co.nz>
2019-04-01 13:17:11 +1300

    use proper recipe title class in <h1> tag

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -51,7 +51,7 @@ export const renderRecipe = recipe => {
     `
     <figure class="recipe__fig">
         <img src="${recipe.img}" alt="${recipe.title}" class="recipe__img">
-        <h1 class="${recipe.title}">
+        <h1 class="recipe__title">
             <span>${recipe.title}</span>
         </h1>
     </figure>
