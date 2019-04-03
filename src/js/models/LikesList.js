1925578ca86a5e92952ad0638d0c485fe73e9c5b
Tom Quinn <tomquinn@hotmail.co.nz>
2019-04-03 22:08:06 +1300

    update localStorage when adding and deleting likes

--- src/js/models/LikesList.js
+++ src/js/models/LikesList.js
@@ -5,14 +5,22 @@ export default class LikesList extends Array {
 
     add(like) {
         this.push(like);
+        
+        this.persistData();
     }
 
     delete(id) {
         const i = this.findIndex(el => el.id === id);
         this.splice(i, 1);
+
+        this.persistData();
     }
 
     isLiked(id) {
         return this.findIndex(el => el.id === id) !== -1;
+    }
+
+    persistData() {
+        localStorage.setItem('likes', JSON.stringify(this));
     }
 }
